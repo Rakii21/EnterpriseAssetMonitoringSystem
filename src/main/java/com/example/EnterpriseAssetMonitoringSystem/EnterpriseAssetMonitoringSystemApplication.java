5755a16e40aea415a40ee02c622c6302f0396e6e
Rakii21 <rakiijaan@gmail.com>
2025-05-27 09:59:24 +0530

    Initial commit

---
+++ src/main/java/com/example/EnterpriseAssetMonitoringSystem/EnterpriseAssetMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnterpriseAssetMonitoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAssetMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAssetMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnterpriseAssetMonitoringSystem/EnterpriseAssetMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnterpriseAssetMonitoringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAssetMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
